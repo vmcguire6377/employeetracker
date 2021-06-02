f856c07dd128b9352fdec03017681845a9336f46
VeeMcGee <vmcguire6377@outlook.com>
2021-06-02 11:31:18 -0600

    creating first query

--- js/inputCheck.test.js
+++ js/inputCheck.test.js
@@ -1,4 +1,4 @@
-const inputCheck = require('../utils/inputCheck');
+pmconst inputCheck = require('../utils/inputCheck');
 
 test('inputCheck() returns null when all properties exist', () => {
   const obj = {name: 'alice'};
--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
-
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //username
+        user: 'root',
+        //sql password
+        password: 'Drm71vmm77!',
+        database: 'employees'
+    },
+    console.log('Connected to the employees database.')
+    );
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+db.query(`SELECT * FROM employees`, (err, rows) => {
+    console.log(rows)
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
