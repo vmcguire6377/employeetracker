07c4c4b148bcf3e249f2f45b1c1ee1391e2fd4aa
VeeMcGee <vmcguire6377@outlook.com>
2021-06-02 11:05:54 -0600

    create employees table, install packages

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    title VARCHAR(30),
+    department VARCHAR(30),
+    salary VARCHAR(30),
+    manager VARCHAR(30)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO employees (
+    first_name, last_name, title, department, salary, manager)
+    VALUES
+    ('Chris', 'Cornell', 'Lead Salesman', 'Sales', '$60,000',
+    'Tom Petty'),
+    ('Lane', 'Staley', 'Marketing Coordinator', 'Marketing',
+    '$70,000', 'Jerry Cantrell'),
+    ('Harry', 'Styles', 'Accountant', 'Finance', '$80,000',
+    'Sug Knight');
