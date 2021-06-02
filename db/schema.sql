06eb1d7b6d63c5957a889cc0dc0cff5f9b5e0535
VeeMcGee <vmcguire6377@outlook.com>
2021-06-02 11:43:25 -0600

    add crud routes

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,6 @@ CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    title VARCHAR(30),
-    department VARCHAR(30),
-    salary VARCHAR(30),
-    manager VARCHAR(30)
+    role_id INT,
+    manager_id INT
 );
--- server.js
+++ server.js
@@ -22,7 +22,28 @@ app.use(express.json());
 db.query(`SELECT * FROM employees`, (err, rows) => {
     console.log(rows)
 });
-
+// Create an employee
+app.post('/api/employees', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'role_id', 'manager_id');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  }); 
+  });
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
